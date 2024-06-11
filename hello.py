d7f2ca80370f69dcbd7ef8cedd6b7d2ed7b27b97
theclich2 <diegobenitomartinez@gmail.com>
2024-06-11 17:05:41 +0200

    Primera Clase: Comentarios y tipos de datos

---
+++ hello.py
@@ -0,0 +1,25 @@
+#Esto es un comentario en python.
+""" 
+    Esto es un comentario
+    en varias lineas
+"""
+#Tipos de datos
+"""
+    Strings
+    Integers
+    Float
+    Booleans
+    List
+    Complejos
+
+"""
+print(type("Soy un String"))
+print(type(10))
+print(type(1.2))
+print(type(True))
+print(type([1,2,3,4]))
+print(type(1+3j))
+
+
+#########################################################################
+print("hola mundo")
