1a0d9189f27fb3b33374713fd753d21f4b0624b5
theclich2 <diegobenitomartinez@gmail.com>
2024-07-08 20:23:40 +0200

    Condicionales

---
+++ 08-Condicionals.py
@@ -0,0 +1,119 @@
+### Condicionals ###
+
+
+my_condition = 5*2
+
+resultado = True
+
+if my_condition >= 10 and my_condition < 20:
+    resultado = True
+    print(resultado)
+else:
+    resultado = False
+    print(resultado)
+
+print("La ejecución continúa")
+
+
+if my_condition >= 10 and my_condition < 20:
+    resultado = True
+    print(resultado)
+elif my_condition == 0:
+    print("No puede ser 0")
+else:
+    resultado = False
+    print(resultado)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
